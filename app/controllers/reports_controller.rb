a82ef639abd560821b155785cb7390b909671202
Raphael de Andrade <rhdeandrade@gmail.com>
2011-10-30 15:52:38 -0200

    Report done and Translated Paages

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,15 +12,21 @@ class ReportsController < ApplicationController
     if params[:city] != "" && params[:neighborhood] != ""
       customers = Customer.where(city: params[:city], neighborhood: params[:neighborhood])
       @sales = []
-      customers.each {|c| @sales += c.sales }
+      date = Time.now
+      date = date - (60 * 60 * 24 * 365 * 2)
+      customers.each {|c| @sales += Sale.where(customer_id: c.id, change_chamber: false, :last_change_on.lte => date )}
     elsif params[:city] != ""
       customers = Customer.where(city: params[:city])
       @sales = []
-      customers.each {|c| @sales += c.sales }
+      date = Time.now
+      date = date - (60 * 60 * 24 * 365 * 2)
+      customers.each {|c| @sales += Sale.where(customer_id: c.id, change_chamber: false, :last_change_on.lte => date )}
     elsif params[:neighborhood] != ""
       customers = Customer.where(neighborhood: params[:neighborhood])
       @sales = []
-      customers.each {|c| @sales += c.sales }
+      date = Time.now
+      date = date - (60 * 60 * 24 * 365 * 2)
+      customers.each {|c| @sales += Sale.where(customer_id: c.id, change_chamber: false, :last_change_on.lte => date )}
     else
       @sales = Sale.all
     end
